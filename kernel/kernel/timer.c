27b9c2176c9f079e05243f0a620f48dea8e844c6
Ivan Rusanov <ivan.b.rusanov@mail.com>
2015-04-14 17:33:47 +0300

    [RFC] [PATCH] [timer] Optimise apply_slack() for size and speed. - http://lkml.iu.edu//hypermail/linux/kernel/1201.3/03172.html

--- kernel/kernel/timer.c
+++ kernel/kernel/timer.c
@@ -791,9 +791,6 @@ EXPORT_SYMBOL(mod_timer_pending);
  * Algorithm:
  *   1) calculate the maximum (absolute) time
  *   2) calculate the highest bit where the expires and new max are different
- *   3) use this bit to make a mask
- *   4) use the bitmask to round down the maximum time, so that all last
- *      bits are zeros
  */
 static inline
 unsigned long apply_slack(struct timer_list *timer, unsigned long expires)
@@ -817,9 +814,7 @@ unsigned long apply_slack(struct timer_list *timer, unsigned long expires)
 
 	bit = find_last_bit(&mask, BITS_PER_LONG);
 
-	mask = (1 << bit) - 1;
-
-	expires_limit = expires_limit & ~(mask);
+	expires_limit = (expires_limit >> bit) << bit;
 
 	return expires_limit;
 }
