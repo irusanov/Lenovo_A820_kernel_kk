0e28943437c1e5862fe81cb8de0e1b528e2ff348
Ivan Rusanov <infraredbg@users.noreply.github.com>
2015-04-24 14:55:19 +0300

    Update proton_control.c
    Add the rest of available frequencies

--- mediatek/platform/mt6589/kernel/core/proton_control.c
+++ mediatek/platform/mt6589/kernel/core/proton_control.c
@@ -4,7 +4,7 @@
 /*********************
 * GPU Frequency List
 **********************/
-#define GPU_DVFS_F1     (806000)   // KHz
+#define GPU_DVFS_F1     (476666)   // KHz
 #define GPU_DVFS_F2     (403000)   // KHz
 #define GPU_DVFS_F3     (357500)   // KHz
 #define GPU_DVFS_F4     (312000)   // KHz
@@ -35,22 +35,20 @@
  * VARIABLES
  ********************************************************************/
 
-// PROTON: GPU frequency control in MHz
+// GPU frequency control in MHz
 unsigned int proton_gpu_frequency = 286;
 module_param(proton_gpu_frequency, int, 0664);
 MODULE_PARM_DESC(proton_gpu_frequency, "Sets the GPU frequency");
-//EXPORT_SYMBOL(proton_gpu_frequency);
 
-// PROTON: GPU voltage control in mV
+// GPU voltage control in mV
 unsigned int proton_gpu_voltage = 1050;
 module_param(proton_gpu_voltage, int, 0664);
 MODULE_PARM_DESC(proton_gpu_voltage, "Sets the GPU voltage");
-EXPORT_SYMBOL(proton_gpu_voltage);
 
-// PROTON: GPU DVFS (1: enable, 0: disable)
+// GPU DVFS switch (1: enable, 0: disable)
 int proton_gpu_dvfs = 0;
 module_param(proton_gpu_dvfs, int, 0664);
-MODULE_PARM_DESC(proton_gpu_dvfs, "Enables or disabled GPU DVFS *Dynamic voltage and frequency)");
+MODULE_PARM_DESC(proton_gpu_dvfs, "Enables or disabled GPU DVFS (Dynamic voltage and frequency)");
 EXPORT_SYMBOL(proton_gpu_dvfs);
 
 /*********************************************************************
@@ -69,22 +67,28 @@ unsigned int proton_gpu_frequency_get(void) {
 	{
 		case 476:
 			freq = GPU_DVFS_F1;
-    		break;
-    	case 403:
+	    	break;
+	    case 403:
 			freq = GPU_DVFS_F2;
-    		break;
-    	case 357:
+	    	break;
+	    case 357:
 			freq = GPU_DVFS_F3;
-    		break;
-    	case 312:
+	    	break;
+	    case 312:
 			freq = GPU_DVFS_F4;
-    		break;
-    	default:
-    	case 286:
+	    	break;
+	    default:
+	    case 286:
 			freq = GPU_DVFS_F5;
-    		break;
-    	case 268:
+	    	break;
+	    case 268:
 			freq = GPU_DVFS_F6;
+			break;
+		case 238:
+			freq = GPU_DVFS_F7;
+			break;
+		case 156:
+			freq = GPU_DVFS_F8;
 			break;
 	}
 	
@@ -103,17 +107,17 @@ unsigned int proton_gpu_tbltype_get(void) {
 	switch (proton_gpu_frequency)
 	{
 		case 476:
-			tbltype = TBLTYPE3;
-    		break;
-    	case 403:
-			tbltype = TBLTYPE2;
-    		break;
-    	case 357:
-			tbltype = TBLTYPE1;
-    		break;
-    	default:
-			tbltype = TBLTYPE0;
-    		break;
+		tbltype = TBLTYPE3;
+			break;
+		case 403:
+		tbltype = TBLTYPE2;
+			break;
+		case 357:
+		tbltype = TBLTYPE1;
+			break;
+		default:
+		tbltype = TBLTYPE0;
+			break;
 	}
 	
 	return tbltype;
