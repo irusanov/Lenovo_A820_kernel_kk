ad1cb973e76908388a47aef4c11d0db97bc5182e
Ivan Rusanov <ivan.b.rusanov@mail.com>
2016-04-08 01:28:18 +0300

    lib: Add support for LZ4 compress, decompress and crypto

--- kernel/include/linux/lz4k.h
+++ kernel/include/linux/lz4k.h
@@ -4,18 +4,17 @@
 
 #include <linux/types.h>
 
-#define LZ4K_TAG 1261722188 // "LZ4K"
+#define LZ4K_TAG 1261722188	/* "LZ4K" */
 
 int lz4k_compress(const unsigned char *src, size_t src_len,
-            unsigned char *dst, size_t *dst_len, void *wrkmem);
+		unsigned char *dst, size_t *dst_len, void *wrkmem);
 
 int lz4k_decompress_safe(const unsigned char *src, size_t src_len,
-            unsigned char *dst, size_t *dst_len);
+		unsigned char *dst, size_t *dst_len);
 
 #ifdef CONFIG_UBIFS_FS
-int lz4k_decompress_ubifs(const unsigned char *src, size_t src_len, 
-            unsigned char *dst, size_t *dst_len);
-#endif // CONFIG_UBIFS_FS
+int lz4k_decompress_ubifs(const unsigned char *src, size_t src_len,
+		unsigned char *dst, size_t *dst_len);
+#endif /* CONFIG_UBIFS_FS */
 
-#endif // __LZ4K_H__
-
+#endif /* __LZ4K_H__ */
