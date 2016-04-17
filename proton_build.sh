aaebb54d2129a60667e85097237869a9bdb68097
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2016-04-18 01:50:50 +0300

    Bump version to 2.54

--- proton_build.sh
+++ proton_build.sh
@@ -9,7 +9,7 @@ if [ -f proton_kernel.conf ]
   else
 	echo "Kernel configuration file (kernel.conf) does not exist!"
 	echo "Using default configuration..."
-	VERSION=2.53
+	VERSION=2.54
 	DEVICE=lenovo_a820
 	UNDERVOLT=no
 	TOOLCHAIN=linaro
