22f94d25be3d4f5d52a12d68596be316e15f9031
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2016-09-09 02:11:34 +0300

    mediatek: Fix "buffer overflow in Mediatek HWMON driver" issue
    https://android.googlesource.com/kernel/mediatek/+/30890de84da3870678450ff1e4b5a7f9afb38b00%5E%21/#F0

--- mediatek/kernel/drivers/hwmon/hwmsen/hwmsen_dev.c
+++ mediatek/kernel/drivers/hwmon/hwmsen/hwmsen_dev.c
@@ -502,6 +502,11 @@ static int hwmsen_enable(struct hwmdev_object *obj, int sensor, int enable)
 
 	sensor_type = 1 << sensor;
 	
+    if (sensor > MAX_ANDROID_SENSOR_NUM) {
+		HWM_ERR("handle %d!\n", sensor);
+		return -EINVAL;
+    }
+
 	if(!obj)
 	{
 		HWM_ERR("hwmdev obj pointer is NULL!\n");
@@ -612,6 +617,11 @@ static int hwmsen_enable_nodata(struct hwmdev_object *obj, int sensor, int enabl
 	HWM_FUN(f);
 	sensor_type = 1 << sensor;
 
+    if (sensor > MAX_ANDROID_SENSOR_NUM) {
+		HWM_ERR("handle %d!\n", sensor);
+		return -EINVAL;
+    }
+
 	if(NULL == obj)
 	{
 		HWM_ERR("hwmdev obj pointer is NULL!\n");
@@ -671,6 +681,11 @@ static int hwmsen_set_delay(int delay, int handle )
 {
 	int err = 0;
 	struct hwmsen_context *cxt = NULL;
+
+    if (handle > MAX_ANDROID_SENSOR_NUM) {
+		HWM_ERR("handle %d!\n", handle);
+		return -EINVAL;
+    }
 
 	cxt = hwm_obj->dc->cxt[handle];
 	if(NULL == cxt ||(cxt->obj.sensor_operate == NULL))
